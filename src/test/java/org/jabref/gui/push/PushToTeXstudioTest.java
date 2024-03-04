f64e87fedc1cdd758c00d861f45b703ba956e481
maxisr <maxism29.mi@gmail.com>
2024-03-04 21:48:36 +0100

    fix: PushToTexstudioTest.java (#10941)

--- src/test/java/org/jabref/gui/push/PushToTeXstudioTest.java
+++ src/test/java/org/jabref/gui/push/PushToTeXstudioTest.java
@@ -7,166 +7,96 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
 
 import org.jabref.gui.DialogService;
-import org.jabref.gui.icon.IconTheme;
-import org.jabref.gui.icon.JabRefIcon;
-import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.push.CitationCommandString;
+import org.jabref.preferences.ExternalApplicationsPreferences;
 import org.jabref.preferences.PreferencesService;
 import org.jabref.preferences.PushToApplicationPreferences;
-import org.jabref.preferences.ExternalApplicationsPreferences;
-import org.jabref.preferences.JabRefPreferences;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
 import org.mockito.Answers;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import org.junit.jupiter.api.Test;
-
-import javafx.beans.property.MapProperty;
-import java.util.Arrays;
-
-
 class PushToTeXstudioTest {
+
+    private static final String TEXWORKS_CLIENT_PATH = "/usr/bin/texstudio";
+    private static final String DISPLAY_NAME = "TeXstudio";
 
     private PushToTeXstudio pushToTeXstudio;
 
     @BeforeEach
     public void setup() {
-
-        // Mock the DialogService and PreferencesService
         DialogService dialogService = mock(DialogService.class, Answers.RETURNS_DEEP_STUBS);
         PreferencesService preferencesService = mock(PreferencesService.class);
         PushToApplicationPreferences pushToApplicationPreferences = mock(PushToApplicationPreferences.class);
-        ExternalApplicationsPreferences externalApplicationsPreferences = mock(ExternalApplicationsPreferences.class);
 
-        // Mock the return value for getCommandPaths()
-        String texstudioClientPath = "/usr/bin/texstudio"; // For linux OS tobe changed in Windows and Mac
-        String displayName = "Texstudio";
-
-        Map<String, String> commandPaths = Map.of(displayName, texstudioClientPath);
+        // Mock the command path
+        Map<String, String> commandPaths = Map.of(DISPLAY_NAME, TEXWORKS_CLIENT_PATH);
         ObservableMap<String, String> observableCommandPaths = FXCollections.observableMap(commandPaths);
         when(pushToApplicationPreferences.getCommandPaths()).thenReturn(new SimpleMapProperty<>(observableCommandPaths));
         when(preferencesService.getPushToApplicationPreferences()).thenReturn(pushToApplicationPreferences);
 
-        //Mock the return value for getCiteCommand()
+        // Mock the return value for getCiteCommand()
+        ExternalApplicationsPreferences externalApplicationsPreferences = mock(ExternalApplicationsPreferences.class);
         CitationCommandString mockCiteCommand = mock(CitationCommandString.class);
-        // when(mockCiteCommand.prefix()).thenReturn("");
-        // when(mockCiteCommand.suffix()).thenReturn("");
-        when(mockCiteCommand.prefix()).thenReturn("[");
-        when(mockCiteCommand.delimiter()).thenReturn(",");
-        when(mockCiteCommand.suffix()).thenReturn("]");
-
+        when(mockCiteCommand.prefix()).thenReturn("");
+        when(mockCiteCommand.suffix()).thenReturn("");
         when(externalApplicationsPreferences.getCiteCommand()).thenReturn(mockCiteCommand);
-
-        // Mock the return value for getExternalApplicationsPreferences()
         when(preferencesService.getExternalApplicationsPreferences()).thenReturn(externalApplicationsPreferences);
 
-        // Create a new instance of PushToTeXstudio
+        // Create a new instance of PushToTeXworks
         pushToTeXstudio = new PushToTeXstudio(dialogService, preferencesService);
-
-        // Verify that the command path is correctly set in the preferences
-        MapProperty<String, String> actualCommandPaths = pushToApplicationPreferences.getCommandPaths();
-        Map<String, String> actualMap = actualCommandPaths.get();
-        String actualPath = actualMap.get(displayName);
-        assertEquals(texstudioClientPath, actualPath);
     }
 
     /**
      * To verify that the PushToTeXstudio class correctly returns its designated display name.
      * The display name is used to identify the application in the GUI.
-     * 
-     * Method: getDisplayName()
-     * 
      */
     @Test
-    void testDisplayName() {
-        // Test whether the display name is correct
-        assertEquals("TeXstudio", pushToTeXstudio.getDisplayName());
+    void displayName() {
+        assertEquals(DISPLAY_NAME, pushToTeXstudio.getDisplayName());
     }
 
     /**
      * To verify that the PushToTeXstudio class correctly returns the command line for TeXstudio.
      * The command line is used to execute the application from the command line.
-     * 
-     * Method: getCommandLine()
-     * 
      */
-    @Disabled("Disabled, Need to add the method to AbstractPushToApplication getCommandLine()") 
     @Test
-    void testGetCommandLine() {
+    void getCommandLine() {
         String keyString = "TestKey";
-        String[] expectedCommand = new String[] {"/usr/bin/texstudio", "--insert-text", "TestKey"};
+        String[] expectedCommand = new String[] {null, "--insert-cite", keyString}; // commandPath is only set in pushEntries
 
-        // pushToTeXstudio.setCommandPath("/usr/bin/texstudio"); // TO add the function
-        
         String[] actualCommand = pushToTeXstudio.getCommandLine(keyString);
 
         assertArrayEquals(expectedCommand, actualCommand);
     }
 
     /**
-     * This test run the external application to push the keys to be cited 
-     * 
-     * Method: pushEntries()
-     * 
-     * [Precondition]: TeXstudio should be running in the background
+     * Check for the actual command and path with path is run.
      */
-    @Disabled("Disabled as it needs running TeXstudio in the background. Start TeXstudio with &")
     @Test
     void pushEntries() {
-        pushToTeXstudio.pushEntries(null, null, "key1,key2");
+        ProcessBuilder processBuilder = mock(ProcessBuilder.class);
+
+        String testKey = "TestKey";
+        String[] expectedCommand = new String[] {TEXWORKS_CLIENT_PATH, "--insert-cite", testKey};
+
+        pushToTeXstudio.pushEntries(null, null, testKey, processBuilder);
+
+        verify(processBuilder).command(expectedCommand);
     }
 
     /**
      * To verify that the PushToTeXstudio class correctly returns the tooltip for TeXstudio.
      * The tooltip is used to display a short description of the application in the GUI.
-     * 
-     * Method: getTooltip()
-     * 
      */
     @Test
-    void testGetTooltip() {
+    void getTooltip() {
         assertEquals("Push entries to external application (TeXstudio)", pushToTeXstudio.getTooltip());
-    }
-
-    /**
-     * To verify that the PushToTeXstudio class correctly returns the Prefix for the citation command.
-     * The prefix is used to identify the start of the citation command.
-     * 
-     * Method: getCitePrefix()
-     * 
-     */
-    @Test
-    void getCitePrefixReturnsExpectedPrefix() {
-        assertEquals("[", pushToTeXstudio.getCitePrefix());
-    }
-
-    /**
-     * To verify that the PushToTeXstudio class correctly returns the delimiter for the citation command.
-     * The delimiter is used to separate the citation keys.
-     * 
-     * Method: getDelimiter()
-     * 
-     */
-    @Test
-    void getDelimiterReturnsExpectedDelimiter() {
-        assertEquals(",", pushToTeXstudio.getDelimiter());
-    }
-
-    /**
-     * To verify that the PushToTeXstudio class correctly returns the Suffix for the citation command.
-     * The suffix is used to identify the end of the citation command.
-     * 
-     * Method: getCiteSuffix()
-     * 
-     */
-    @Test
-    void getCiteSuffixReturnsExpectedSuffix() {
-        assertEquals("]", pushToTeXstudio.getCiteSuffix());
     }
 }
